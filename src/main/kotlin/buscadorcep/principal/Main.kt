d9b3159080b008cfd938bd1386ad06edd280285c
Lucas <lucas.carrijoferrari@gmail.com>
2024-05-26 23:24:18 -0300

    novos conceitos e scripts

--- src/main/kotlin/buscadorcep/principal/Main.kt
+++ src/main/kotlin/buscadorcep/principal/Main.kt
@@ -6,9 +6,10 @@ import java.util.Scanner
 fun main() {
     val entrada = Scanner(System.`in`)
 
+    println("Digite o seu CEP: ")
     val cepParaBuscar = entrada.nextLine()
 
     val buscador = buscador()
-    buscador.buscaCep(cepParaBuscar)
+    buscador.jsonParaClassEndereco(cepParaBuscar)
 
 }
--- src/main/kotlin/buscadorcep/servicos/buscador.kt
+++ src/main/kotlin/buscadorcep/servicos/buscador.kt
@@ -1,23 +1,39 @@
 package buscadorcep.servicos
 
+import com.google.gson.Gson
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 import java.net.http.HttpResponse.BodyHandlers
 
 
 class buscador {
-    fun buscaCep(cep: String){
-        println("hi")
+
+    fun buscaCep(cep: String): String? {
+        val endereco = "https://viacep.com.br/ws/$cep/json/"
+
         val client: HttpClient = HttpClient.newHttpClient()
         val request = HttpRequest.newBuilder()
-            .uri(URI.create("https://viacep.com.br/ws/$cep/json/"))
+            .uri(URI.create(endereco))
             .build()
-        client.sendAsync(request, BodyHandlers.ofString())
-            .thenApply { obj: HttpResponse<*> -> obj.body() }
-            .thenAccept(System.out::println)
-            .join()
 
+        val response =
+            client.send(request, BodyHandlers.ofString())
+
+        val json = response.body()
+
+        return json
+    }
+
+    fun jsonParaClassEndereco(cep: String) {
+        runCatching {
+            val json = buscaCep(cep)
+            val gson = Gson()
+            gson.fromJson(json, endereco::class.java)
+        }.onSuccess { enderecoObjeto ->
+            println(enderecoObjeto)
+        }.onFailure { ex ->
+            println("Erro ao buscar ou converter o CEP: ${ex.message}")
+        }
     }
 }
